849afc9203ebf4922199541a3f59b1746f1c55e0
Ujjwal Sonawat <ujjwalsonawat7@gmail.com>
2024-11-12 23:06:13 +0530

    done

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/ecommerce_by_me")
+mongoose.connect(process.env.DB_URL)
 .then(()=> {
     console.log("Connected To Database");
+})
+.catch((err)=> {
+    console.log(err.message);
 });
 
 module.exports = mongoose.connection;
