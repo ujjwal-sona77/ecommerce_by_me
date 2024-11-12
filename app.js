c873d5b7bf05c433e8da2fa9a3e289a1b4896fdd
Ujjwal Sonawat <ujjwalsonawat7@gmail.com>
2024-11-12 21:06:19 +0530

    added signup page

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const indexRouter = require('./routes/indexRouter');
+const usersRouter = require("./routes/userRouter");
+const ownersRouter = require("./routes/ownerRouter");
+const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
+app.use("/" , indexRouter);
+app.use("/users" , usersRouter);
+app.use("/owner" , ownersRouter)
+
+app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000');
 });
